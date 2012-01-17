16e585fdd18f47fd53e286841fa04c0b9ad516bb
James Wade <web@hm2k.org>
2012-01-17 21:43:05 +0000

    fixes to the admin news

--- web/htdocs/app/admin_news.html.php
+++ web/htdocs/app/admin_news.html.php
@@ -10,7 +10,7 @@ if (isset($_GET['add'])) {
 
     $date = date("Y-m-d", time());
     $db->query("INSERT INTO news (title, date, text) VALUES
-    ('$_POST[title]','$date','$_POST[text]')") or die(mysql_error());
+    ('$_POST[title]','$date','$_POST[text]')") or die($db->error());
 
 } elseif (isset($_GET['edit'])) {
 
@@ -24,9 +24,9 @@ if (isset($_GET['add'])) {
 <table width="575" cellpadding="0" cellspacing="0" border="0">
  <tr>
   <td class="text" colspan="3" align="center">
-   <? if (!isset($_GET['new'])) { ?>
+   <?php if (!isset($_GET['new'])) { ?>
     <a style="color: green" href="admin_news?new=1">Post new</a><br />
-   <? } ?>
+   <?php } ?>
     <br />
   </td>
  </tr>
@@ -34,20 +34,20 @@ if (isset($_GET['add'])) {
   <td>
 
 <table border="0" cellpadding="0" cellspacing="0" width="400">
-<?
+<?php
 if (!isset($_GET['start'])) { $start = 0; }
 if (!isset($_GET['end'])) { $end = 20; }
 
 $query = $db->query("SELECT * FROM news ORDER BY date DESC LIMIT $start,$end");
 
-while ($query as $row) {
+foreach ($query as $row) {
     ?>
  <tr>
   <td class="text"><a href="admin_news?id=<?=$row['id']?>"><?=$row['title']?></a></td>
   <td class="text" align="left" style="color: gray"><?=$row['date']?></td>
  </tr>
 
-<? } ?>
+<?php } ?>
 </table>
 </td>
 <td width="25">&nbsp;</td>
@@ -55,10 +55,10 @@ while ($query as $row) {
 
 <table border="0" cellpadding="0" cellspacing="0" width="150" align="right">
 <tr>
-<?
-$query = $db->query("SELECT * FROM news") or die(mysql_error());
+<?php
+$query = $db->query('SELECT * FROM news');
 
-$rows = mysql_num_rows($query);
+$rows = count($query);
 
 if ($rows > $end) {
     $prevstart = $start - 20;
@@ -76,48 +76,48 @@ if ($rows > $end) {
 
 <br />
 
-<? if ($id) {
-    $query = mysql_query("SELECT * FROM news WHERE id='$id'");
+<?php if ($id) {
+    $query = $db->query('SELECT * FROM news WHERE id='.$id);
 
-    while ($row = mysql_fetch_array($query)) {
+    foreach ($query as $row) {
 ?>
 <b>Edit</b><br />
-<form method="post" action="admin_news?edit=1&id=<? print $row['id']; ?>">
-Title: <input type="text" name="title" value="<? print $row['title']; ?>" size="30"><br />
+<form method="post" action="admin_news?edit=1&id=<?=$row['id']; ?>">
+Title: <input type="text" name="title" value="<?=$row['title']; ?>" size="30"><br />
 
-<?
+<?php
 $dd = substr($row['date'], 8, 2);
 ?>
 
 Day:
 
 <select name="dd">
-<? for($i=1;$i <= 31;$i++) { if(strlen($i) < 2) $i = "0".$i; ?>
-<option value="<? print $i ?>" <? if($dd == "$i") { print "selected"; } ?>><? print $i ?></option>
-<? } ?>
+<?php for($i=1;$i <= 31;$i++) { if(strlen($i) < 2) $i = "0".$i; ?>
+<option value="<?=$i ?>" <?php if($dd == "$i") { print "selected"; } ?>><?=$i ?></option>
+<?php } ?>
 </select>
 
 
-<?
+<?php
 $mm = substr($row[date], 5, 2);
 ?>
 
 Month:
 <select name="mm">
-<? for($i=1;$i <= 12;$i++) { if(strlen($i) < 2) $i = "0".$i; ?>
-<option value="<? print $i ?>" <? if($mm == "$i") { print "selected"; } ?>><? print $i ?></option>
-<? } ?>
+<?php for($i=1;$i <= 12;$i++) { if(strlen($i) < 2) $i = '0'.$i; ?>
+<option value="<?=$i ?>" <?php if($mm == "$i") { print "selected"; } ?>><?=$i ?></option>
+<?php } ?>
 </select>
 
 Year:
-<input type="text" name="yyyy" size="4" maxlength="4" value="<? print
+<input type="text" name="yyyy" size="4" maxlength="4" value="<?php print
 substr($row['date'], 0, 4); ?>"><br />
-Text: <textarea name="text" rows="10" cols="80"><? echo $row['text']; ?></textarea><br />
+Text: <textarea name="text" rows="10" cols="80"><?php echo $row['text']; ?></textarea><br />
 <input type="submit" name="Edit" value="Edit">
 </form>
-<? } }  ?>
+<?php } }  ?>
 <br />
-<? if (isset($_GET['new'])) { ?>
+<?php if (isset($_GET['new'])) { ?>
 <b>Post new</b><br />
 <form method="post" action="admin_news?add=1">
 <input type="hidden" name="date">
@@ -125,4 +125,4 @@ Title: <input type="text" name="title" size="30"><br />
 Text: <textarea name="text" rows="10" cols="80"></textarea><br />
 <input type="submit" name="Add new" value="Add new">
 </form>
-<? } ?>
+<?php } ?>
