53c5076bc2ea500e7405b7e79975da8250c5197b
James Wade <web@hm2k.org>
2012-01-18 14:12:35 +0000

    modified downloads page with new details

--- web/htdocs/app/download.html.php
+++ web/htdocs/app/download.html.php
@@ -1,31 +1,32 @@
 <h3>Available downloads</h3>
-
-<a href="http://prdownloads.sourceforge.net/pisg/pisg-<?=$release_version?>.tar.gz">pisg-<?=$release_version?>.tar.gz</a> - Unix tarball<br />
-<a href="http://prdownloads.sourceforge.net/pisg/pisg-<?=$release_version?>.zip">pisg-<?=$release_version?>.zip</a> - Zip file <br />
-<br />
-Older releases are found <a href="http://sourceforge.net/project/showfiles.php?group_id=31862">here</a>
+<ul>
+    <li><a href="http://prdownloads.sourceforge.net/pisg/pisg-<?=$release_version?>.tar.gz">pisg-<?=$release_version?>.tar.gz</a> - Unix tarball</li>
+    <li><a href="http://prdownloads.sourceforge.net/pisg/pisg-<?=$release_version?>.zip">pisg-<?=$release_version?>.zip</a> - Zip file </li>
+</ul>
+<p>Older releases are found <a href="http://sourceforge.net/project/showfiles.php?group_id=31862">here</a></p>
 
 <h3>Debian package</h3>
-pisg is available as an <a href="http://packages.debian.org/pisg">official
+<p>pisg is available as an <a href="http://packages.debian.org/pisg">official
 Debian package</a> in the Debian archives. It can be installed using
-<b>apt-get</b>.
+<b>apt-get</b></p>
 
 <h3>FreeBSD port</h3>
-
-pisg has an <a
+<p>pisg has an <a
 href="http://www.freebsd.org/cgi/url.cgi?ports/irc/pisg/pkg-descr">official
-FreeBSD port</a> package.
+FreeBSD port</a> package.</p>
 
 <h3>Mac OS X fink package</h3>
-pisg is available as a <a
+<p>pisg is available as a <a
 href="http://fink.sourceforge.net/pdb/package.php/pisg">fink package</a>,
-installable via the <a href="http://fink.sourceforge.net">Fink project</a>
+installable via the <a href="http://fink.sourceforge.net">Fink project</a></p>
 
-<h3>Anonymous CVS</h3>
-You can also download the latest development version of pisg via CVS. On
-a Unix-like system, it can be done like this:
+<h3>Git</h3>
+<p>You can also download the latest development version of pisg via the Git
+repositry.</p>
+
+<p>For read only access:</p>
 <pre>
-cvs -d:pserver:anonymous@pisg.cvs.sourceforge.net:/cvsroot/pisg login
- 
-cvs -z3 -d:pserver:anonymous@pisg.cvs.sourceforge.net:/cvsroot/pisg co pisg
+git://pisg.git.sourceforge.net/gitroot/pisg/pisg
 </pre>
+
+<p><em>Note: CVS access has been removed.</em></p>
