25951925c0aa8558d2bbbc3ce31dde6452f54695
James Wade <web@hm2k.org>
2012-01-23 17:07:28 +0000

    cleanup about page

--- web/htdocs/app/about.html.php
+++ web/htdocs/app/about.html.php
@@ -1,52 +1,45 @@
 <h3>What exactly is pisg?</h3>
 
-pisg is a so called "logfile analyzer" - it takes an IRC logfile,
-analyzes it, and generates some statistics from it.<br />
-A lot of people find these stats amusing. That's really the main reason
-of making the stats.<br />
-<br />
-What you need to do exactly to generate these stats, is to:
+<p>pisg is called a "logfile analyzer" - it takes an IRC logfile,
+analyzes it, and generates some statistics from it.</p>
+</p>A lot of people find these stats amusing. That's really the main reason
+of making the stats.</p>
+
+<p>What you need to do exactly to generate these stats, is to:</p>
 <ul>
     <li>Use a IRC client or bot to generate a logfile</li>
     <li>Make pisg run through your logfile</li>
     <li>Upload the HTML file to a website so everyone can see it (see <a
         href="examples">examples here</a>)</li>
 </ul>
-<br />
 
-It's that simple :)<br />
-<br />
-Of course, you can also automate all these things with a scheduling
-program like crontab, so you don't have to manually update the stats.
+<p>It's that simple :)</p>
+
+<p>Of course, you can also automate all these things with a scheduling
+program like crontab, so you don't have to manually update the stats.</p>
 
 <h3>Who is developing pisg?</h3>
-pisg was created by <b>Morten Brix Pedersen (mbp)</b> as a small project which helped a bit on the motivation to
-learn Perl.<br /> Since then, a lot of people has contributed with their
-ideas and code, see the CREDITS file in the source for the list of people
-who has contributed.<br />
-<br />
-Patches are highly appreciated, mail your ideas to the pisg
-<a href="list">mailing list</a>!
+<p>pisg was created by <b>Morten Brix Pedersen (mbp)</b> as a small project which
+helped a bit on the motivation to learn Perl.</p>
+<p>Since then, a lot of people has contributed with their ideas and code, see the CREDITS file in the source for the list of people
+who has contributed.</p>
+<p>Patches are highly appreciated, mail your ideas to the pisg <a href="list">mailing list</a>!</p>
 
 
 <h3>Adding support for new logfile formats</h3>
-If you have a fair amount of Perl knowledge, it is not very hard to add
-support for a new logfile format (clients, bots, loggers).<br />
-<br />
-If you don't have any Perl knowledge, you can try sending a sample logfile
-to the <a href="list">mailing list</a> and see if anyone is kind enough to write one for you.
+<p>If you have a fair amount of Perl knowledge, it is not very hard to add
+support for a new logfile format (clients, bots, loggers).</p>
+<p>If you don't have any Perl knowledge, you can try sending a sample logfile
+to the <a href="list">mailing list</a> and see if anyone is kind enough to write one for you.</p>
 
-<h3>pisg translations</h3>
-Pisg is translated into about every language that you can think of,
-including:<br />
-<br />
-English, German, Norweigan, Portugese, Danish, French, Spanish, Polish,
+<h3>Translations</h3>
+<p>Pisg is translated into about every language that you can think of,
+including:</p>
+<p>English, German, Norweigan, Portugese, Danish, French, Spanish, Polish,
 Dutch, Swedish, Finnish, Slovenian, Hungarian, Estonian, Italian,
 Catalan, Turkish, Romanian, Czech, Icelandic, Russian, Hebrew,
-Bulgarian, Greek, Flemish, Albanian, Serbian and Slovak. <br />
-<br />
-You're very welcome to translate pisg into your own language, it's not
-very hard.
+Bulgarian, Greek, Flemish, Albanian, Serbian and Slovak.</p>
+<p>You're very welcome to translate pisg into your own language, it's not very hard.</p>
 
 <h3>Other IRC statistics generators</h3>
 <ul>
